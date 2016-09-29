bf1a1a72e8a6d6fc51cbc0923303fa4a0e841cf0
abhikerni <abhikerni2025@gmail.com>
2016-09-29 21:19:51 +0530

    Incomplete ode

--- Algorithim3/src/Evacuation.java
+++ Algorithim3/src/Evacuation.java
@@ -3,11 +3,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Evacuation {
     private static FastScanner in;
     static Graph graph = new Graph();
     static Graph residualGraph= new Graph();
+    static ArrayList<Integer> path;
     
     public static void main(String[] args) throws IOException {
         in = new FastScanner();
@@ -19,12 +21,8 @@ public class Evacuation {
         
         
         System.out.println(maxFlow(graph,residualGraph));
-        
-        int parent=residualGraph.nodes.get(residualGraph.nodes.size()-1).parentNode;
-        while(parent!=0)
-        {System.out.println(parent+1);
-         parent=residualGraph.nodes.get(parent).parentNode;
-        }
+
+         System.out.println(path);
     }
 
     private static int maxFlow(Graph graph,Graph residualGraph) {
@@ -33,14 +31,61 @@ public class Evacuation {
         
         Node root=residualGraph.nodes.get(0);
         int target=residualGraph.nodes.size()-1;
+        
         pathFound=findPath(root,target,false);
         
+        
+        int minFlow=findMinFlow(residualGraph);
+        
+        addFlow(minFlow,graph,residualGraph);
+        
+        
+        System.out.println("MinFlow:"+minFlow);
         System.out.println(pathFound);
         
         return flow;
     }
 
-    private static boolean findPath(Node root,int target,boolean found)
+    private static void addFlow(int minFlow, Graph graph, Graph residualGraph)
+	{
+		
+		
+	}
+    
+    
+    
+	private static int findMinFlow(Graph residualGraph)
+	{
+		path=new ArrayList<Integer>();
+		
+		
+    	int target=residualGraph.nodes.size()-1;
+    	Node targetNode=residualGraph.nodes.get(target);
+    	int parent=targetNode.parentNode;
+    	
+    	
+    	int flow=residualGraph.getFlow(parent,target);
+    	
+    	path.add(target);
+    	while(parent!=0)
+    	{
+    		target=parent;
+    		parent=residualGraph.nodes.get(parent).parentNode;
+    		
+    		int newFlow=residualGraph.getFlow(parent,target);
+    		if(newFlow<flow)
+    			flow=newFlow;
+    		
+    		path.add(target);
+    	}
+    	
+    	path.add(0);
+    	Collections.reverse(path);
+		return flow;
+		
+	}
+
+	private static boolean findPath(Node root,int target,boolean found)
 	{
        
     	
@@ -118,6 +163,16 @@ public class Evacuation {
         {
         	this.nodeNumber=nodeNumber;
         }
+        
+        public void changeFlow(int to,int flow)
+        {
+        	for(Edge edge:edges)
+        	{
+        		if(edge.to==to)
+        			edge.flow=flow;
+        	}
+        }
+        
     }
     
     static class Graph
@@ -128,6 +183,34 @@ public class Evacuation {
     	{
     		nodes.get(from).edges.add(new Edge(to,capacity,flow));
     	}
+
+		public int getFlow(int parent, int target)
+		{
+			Node from=nodes.get(parent);
+			for(Edge edge:from.edges)
+			{
+				if(edge.to==target)
+					return edge.flow;
+			}
+			
+			return 0;
+		}
+		
+		
+		public void newFlow(int from,int to,int flow)
+		{
+			
+			Node fromNode=nodes.get(from);
+			Edge newEdge=new Edge(from,to,flow);
+			for(Edge edge:fromNode.edges)
+			{
+				if(edge.to==to)
+					nodes.remove(edge);
+			}
+			
+			fromNode.edges.add(newEdge);
+		}
+		
     }
     
 
