11870fb4b1f560958c61504d08292018976436ef
killer2020 <abhikerni2025@gmail.com>
2017-06-19 14:19:35 +0530

    nothing working

--- HackerRank2/src/MaximumSubArraySum.java
+++ HackerRank2/src/MaximumSubArraySum.java
@@ -2,12 +2,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class MaximumSubArraySum
 {
 
-	private static class Node
+	private static class Node implements Comparable<Node>
 	{
 		
 		long data;
@@ -19,13 +22,21 @@ public class MaximumSubArraySum
 		{
 			this.data=data;
 		}
+
+		
+
+		@Override
+		public int compareTo(Node o) {
+			if(o.data==this.data)
+				return 0;
+			return o.data>this.data?-1:1;
+		}
 		
 		
 	}
 	
 	
-	static Node[] arr;
-	static long[] originalData;
+	
 
 	
 	public static void main(String args[]) throws Exception
@@ -44,24 +55,48 @@ public class MaximumSubArraySum
 			
 			long max=0;
 			
-			arr=new Node[size];
-			
-			ArrayList<Node> nodesList=new ArrayList<Node>();
 			
 			
-			originalData[0]=scanner.nextLong()%mod;
-			Node node=new Node(originalData[0]);
-			nodesList.add(node);
+		    long[] arr=new long[size];
 			
+		    Node[] prefixSum=new Node[size];
+		    
+		    
 			for(int j=0;j<size;j++)
 			{
 				
-				originalData[j]=scanner.nextLong()%mod;
+			   arr[j]=scanner.nextLong()%mod;	
 				
+			}
+			
+			prefixSum[0]=new Node(arr[0]);
+			
+			for(int j=1;j<size;j++)
+			{
+				prefixSum[j]=new Node(arr[j]+prefixSum[j-1].data);
+			}
+			for(int j=0;j<size;j++)
+			{
+				prefixSum[j].data=prefixSum[j].data%mod;
 			}
 			
 			
 			
+			ArrayList<Node> sortedData=new ArrayList<Node>(Arrays.asList(prefixSum)); 
+			
+			for(int j=0;j<size;j++)
+			{
+				System.out.println(sortedData.get(j).data);
+			}
+			
+			Collections.sort(sortedData);
+			
+			
+			for(int j=0;j<size;j++)
+			{
+				System.out.println(sortedData.get(j).data);
+				
+			}
 			
 			System.out.println(max);
 		}
@@ -106,15 +141,150 @@ public class MaximumSubArraySum
 	   
    }
 
+   
+   
+   
+   
+   
    private static void removeNode(Node node,Node rootNode)
    {
+	   if(!rootNode.equals(node))
+	   {
+		   if(node.data<rootNode.data)
+			   removeNode(node,rootNode.left);
+		   else
+			  removeNode(node,rootNode.right); 
+		   
+	   }
 	   
-	   //if(node.equals(rootNode))
+	   
+	   if(hasNoChildNode(node))
+	   {
+		   if(isNodeLeftChild(node, rootNode))
+		      rootNode.left=null;
+		   else
+			   if(isNodeRightChild(node, rootNode))
+				   rootNode.right=null;
+		
+		   return;
+	   }
+
+	   
+	   if(hasOneChild(node))
+	   {
+		   adjustSingleChild(node,rootNode);
+		   return;
+	   }
+	   
+	   
+	   if(hasBothChild(node))
+	   {
+		   
+		   
+		   if(node.left.right==null)
+		   {
+			   
+			   if(isNodeLeftChild(node, rootNode))
+			   {
+				   
+			   }
+			   
+		   }
+		   
+		   
+	   }
+	   
+	   
 	   
    }
 
 	
 	
+	private static boolean hasBothChild(Node node) {
+	
+		return (node.left!=null && node.right!=null);
+}
+
+
+	private static void adjustSingleChild(Node node, Node rootNode) 
+	{
+	
+		if(node.left!=null)
+		{
+			
+			if(isNodeLeftChild(node,rootNode))
+			{
+				rootNode.left=node.left;
+				node.left.parent=rootNode;
+			}
+			else
+			if(isNodeRightChild(node, rootNode))
+			{
+				rootNode.right=node.left;
+				node.left.parent=rootNode;
+			}
+			
+		}
+		else
+		if(node.right!=null)
+		{
+			
+			if(isNodeLeftChild(node, rootNode))
+			{
+				rootNode.left=node.right;
+				node.right.parent=rootNode;
+			}
+			else
+			if(isNodeRightChild(node, rootNode))
+			{
+				rootNode.right=node.right;
+				node.right.parent=rootNode;
+				
+			}
+			
+		}
+	
+    }
+
+
+	private static boolean isNodeLeftChild(Node node, Node rootNode) {
+		
+		if(rootNode.left.equals(node))
+			return true;
+		
+		return false;
+		
+	}
+
+	
+    private static boolean isNodeRightChild(Node node, Node rootNode) {
+    		
+		if(rootNode.right.equals(node))
+			return true;
+		
+		return false;
+		
+	}
+
+	private static boolean hasOneChild(Node node) 
+	{
+              
+		if((node.left!=null && node.right==null) || (node.right!=null && node.left==null))
+			return true;
+		
+		
+		return false;
+		
+    }
+
+
+	private static boolean hasNoChildNode(Node node) 
+	{
+	 return (node.left==null && node.right==null);
+    }
+
+
+
 	static class FastScanner {
 	    private BufferedReader reader;
 	    private StringTokenizer tokenizer;
