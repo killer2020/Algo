d62a982d2b13645824eff46c539e8bdbecd0a3b2
killer2020 <abhikerni2025@gmail.com>
2017-06-20 14:23:21 +0530

    good

---
+++ HackerRank2/src/SubArray.java
@@ -0,0 +1,174 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class SubArray
+{
+
+	private static class Node implements Comparable<Node>
+	{
+		
+		long data;
+		
+		public Node(long data)
+		{
+			this.data=data;
+		}
+
+		
+
+		@Override
+		public int compareTo(Node o) {
+			if(o.data==this.data)
+				return 0;
+			return o.data>this.data?-1:1;
+		}
+		
+		
+	}
+	
+	
+	
+	private static ArrayList<Node> sortedData;
+	
+	public static void main(String args[]) throws Exception
+	{
+		
+		FastScanner scanner=new FastScanner();
+		
+		
+		int queries=scanner.nextInt();
+		
+		for(int i=0;i<queries;i++)
+		{
+			
+			int size=scanner.nextInt();
+			long mod=scanner.nextLong();
+			
+			long max=0;
+			
+			
+			
+		    long[] arr=new long[size];
+			
+		    Node[] prefixSum=new Node[size];
+		    
+		    
+			for(int j=0;j<size;j++)
+			{
+				
+			   arr[j]=scanner.nextLong()%mod;	
+				
+			}
+			
+			prefixSum[0]=new Node(arr[0]);
+			
+			for(int j=1;j<size;j++)
+			{
+				prefixSum[j]=new Node(arr[j]+prefixSum[j-1].data);
+			}
+			for(int j=0;j<size;j++)
+			{
+				prefixSum[j].data=prefixSum[j].data%mod;
+			}
+			
+			
+			
+			sortedData=new ArrayList<Node>(Arrays.asList(prefixSum)); 
+			
+			
+			Collections.sort(sortedData);
+			
+			long required=mod-1;
+			
+			long maxInSegment=findNumberlessThan(required,0,arr.length-1);
+			
+			if(maxInSegment>max)
+				max=maxInSegment;
+			
+			System.out.println(max);
+		}
+		
+		
+		
+		
+	}
+
+
+	
+	
+	
+	
+	
+	
+
+	private static long findNumberlessThan(Long data,int start,int end)
+	{
+		int mid=(start+end)/2;
+		
+		if(sortedData.get(mid).data==data)
+		{
+			return sortedData.get(mid).data;
+		}
+		
+		if(start>=end)
+		{
+			    if(start==0 && sortedData.get(mid).data>data)
+			    	return 0;
+
+			    if(sortedData.get(mid).data>data)
+			    	return sortedData.get(mid-1).data;
+			    
+			    return sortedData.get(mid).data;
+			
+		}
+		
+		
+		if(sortedData.get(mid).data>data)
+			return findNumberlessThan(data,start,mid-1);
+		else
+			return findNumberlessThan(data,mid+1,end);
+	}
+	
+
+   
+   
+   
+ 
+	
+	
+	
+
+
+
+
+
+	static class FastScanner {
+	    private BufferedReader reader;
+	    private StringTokenizer tokenizer;
+
+	    public FastScanner() {
+	        reader = new BufferedReader(new InputStreamReader(System.in));
+	        tokenizer = null;
+	    }
+
+	    public String next() throws IOException {
+	        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+	            tokenizer = new StringTokenizer(reader.readLine());
+	        }
+	        return tokenizer.nextToken();
+	    }
+
+	    public int nextInt() throws IOException {
+	        return Integer.parseInt(next());
+	    }
+	    public long nextLong() throws IOException {
+	        return Long.parseLong(next());
+	}
+}
+}
