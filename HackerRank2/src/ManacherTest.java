3e78031485d5dc7a90f8ede9fdd0f187ca25fdeb
abhikerni <abhikerni2025@gmail.com>
2017-07-16 20:55:57 +0530

    Manacher algo linear time palindrome

--- HackerRank2/src/ManacherTest.java
+++ HackerRank2/src/ManacherTest.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Manacher
+public class ManacherTest
 {
 
 	private static String testString="#";
@@ -11,24 +11,21 @@ public class Manacher
 	private static int center=0;
 	private static int radius=0;
 	
+
+	
 	public static void main(String args[]) throws Exception
 	{
-		
-		FastScanner scanner=new FastScanner();
-		int len=scanner.nextInt();
-		String str=scanner.next();
-		
-		for(int j=0;j<len;j++)
+		while(true)
 		{
-			if(j!=0)
-		    {char first=str.charAt(0);
-		    str=str.substring(1);
-			str=str+first;
-		    }
+			
 			testString="#";
 			center=0;
 			radius=0;
-			
+			bruteRadius=0;
+		String str=getRandomString();
+		
+		System.out.println(str);
+		
 		for(int i=0;i<str.length();i++)
 		{
 			testString=testString+str.charAt(i);
@@ -69,18 +66,74 @@ public class Manacher
 				
 			}
 			
-		
+			
 		}
 		
 		
 //		System.out.println(testString);
 //		System.out.println("Radius:"+radius);
 //		System.out.println("Center:"+center);
-		
+//		
 		System.out.println(radius);
-		}
+		
+		
+		for(int i=1;i<testString.length()-1;i++)
+		{
+				int left=i-1;
+				int right=i+1;
+				int localRadius=0;
+				calcPalindromeAroundBrute(i,left,right,localRadius);
+			
+		}	
+	
+		
+		System.out.println(bruteRadius);
+		
+		if(radius!=bruteRadius)
+			return;
+		
+		}	
 	}
 	
+	private static String getRandomString()
+	{
+		int len=(int) (Math.random()*100)+1;
+		
+		char[] set={'a','b','c','d','e','f','g','h','i','j'};
+		
+		String str="";
+		
+		for(int i=0;i<len;i++)
+		{
+			
+			int next=(int) (Math.random()*100)+1;
+			str=str+set[next%set.length];
+		}
+		
+		return str;
+	}
+
+	private static int bruteRadius=0;
+	
+	private static void calcPalindromeAroundBrute(int i,int left,int right,int localRadius)
+	{
+		
+		
+		while((left>=0 && right<testString.length()) && testString.charAt(left)==testString.charAt(right))
+		{
+			localRadius++;
+			left--;
+			right++;
+		}
+		
+		if(localRadius>=bruteRadius)
+		{
+			bruteRadius=localRadius;
+			
+		}
+		
+		
+	}
 	
 	private static void calcPalindromeAround(int i,int left,int right,int localRadius)
 	{
