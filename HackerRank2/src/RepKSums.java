b0b269eb4aee4a5c5b99db71fae8d60ec34554e6
killer2020 <abhikerni2025@gmail.com>
2017-09-08 09:23:35 +0530

    runs slow

--- HackerRank2/src/RepKSums.java
+++ HackerRank2/src/RepKSums.java
@@ -2,27 +2,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class RepKSums
 {
 	
-	
+	private static List<Long> sumList;
 	
 	public static void main(String args[]) throws Exception
 	{
 		FastScanner scanner=new FastScanner();
 		
-		int n=scanner.nextInt();
+		int numberOfTerms=scanner.nextInt();
 		
 		int k=scanner.nextInt();
 		
-		int noOfTerms=0;
+		int noOfSumTerms=0;
 		
-       int arr[][]=new int[k][n];
+       int arr[][]=new int[k][numberOfTerms];
        
-       for(int i=0;i<n;i++)
+       for(int i=0;i<numberOfTerms;i++)
        {
     	   arr[0][i]=1;
        }
@@ -35,22 +36,22 @@ public class RepKSums
        for(int i=1;i<k;i++)
        {
     	   
-    	   for(int j=1;j<n;j++)
+    	   for(int j=1;j<numberOfTerms;j++)
     		   arr[i][j]=arr[i-1][j]+arr[i][j-1];
     	   
        }
        
        
-       for(int i=0;i<n;i++)
-    	   noOfTerms=noOfTerms+arr[k-1][i];
+       for(int i=0;i<numberOfTerms;i++)
+    	   noOfSumTerms=noOfSumTerms+arr[k-1][i];
        
-       System.out.println("No. Of Terms:"+noOfTerms);
+       System.out.println("No. Of Terms:"+noOfSumTerms);
        
        
-       List<Long> sumList=new ArrayList<Long>();
+       sumList=new ArrayList<Long>();
        long sumOfN=0;
        
-       for(int i=0;i<noOfTerms;i++)
+       for(int i=0;i<noOfSumTerms;i++)
        {
     	   
     	   long next=scanner.nextInt();
@@ -59,13 +60,69 @@ public class RepKSums
     	   sumOfN=sumOfN+next;
     	   
        }
+       
+       Collections.sort(sumList);
 
-       sumOfN=(sumOfN*n)/(noOfTerms*k);
+       sumOfN=(sumOfN*numberOfTerms)/(noOfSumTerms*k);
        
        System.out.println("Sum Of Terms:"+sumOfN);
        
+       findAndDeleteKSums(numberOfTerms,k);
        
+       
+       System.out.println(elements);
 	}
+	
+	private static List<Long> elements=new ArrayList<Long>();
+	
+	
+	
+	private static void findAndDeleteKSums(int n,int k)
+	{
+		long firstSum=sumList.get(0);
+		sumList.remove(0);
+		
+		long firstElement=firstSum/k;
+		elements.add(firstElement);
+		
+		while(!sumList.isEmpty())
+		{long nextSum=sumList.get(0);
+		 sumList.remove(0);
+		
+		 long nextElement=nextSum-(firstElement*(k-1));
+		 elements.add(nextElement);
+		 
+		
+		 createKSums(nextElement,k-1,0);
+		}
+		
+	}
+	
+	private static void removeElement(long element)
+	{
+		
+		sumList.remove(element);
+		
+	}
+
+	
+	private static void createKSums(long firstElement,int k,int start)
+	{
+		if(k==0)
+		{	removeElement(firstElement);
+		    return;
+		}
+		for(int i=start;i<(elements.size());i++)
+		{
+			long nextElement=firstElement+elements.get(i);
+			createKSums(nextElement,k-1,i);
+		}
+		
+		
+		
+		
+	}
+	
 	
 	static class FastScanner {
 	    private BufferedReader reader;
